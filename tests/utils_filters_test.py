fcb524efa679b3deedd098ec72f13cde395dae12
Ringil <91410075+rin-gil@users.noreply.github.com>
2023-05-11 17:19:55 +0300

    fix: update utils_filters_test.py

--- tests/utils_filters_test.py
+++ tests/utils_filters_test.py
@@ -22,7 +22,7 @@ class IsAdminFilterTestCase(TestCase):
 
     def test_filter_returns_true_if_user_id_is_admin(self) -> None:
         """Filter test if the sender of the message is a bot administrator"""
-        env.get_admin_ids = MagicMock(return_value=[self._admin_id])
+        env.get_admin_ids = MagicMock(return_value=[self._admin_id])  # type: ignore
         user: User = User(id=self._admin_id, first_name="Admin", is_bot=False)
         message: Message = Message(message_id=1, from_user=user, date=self._date, chat=self._chat)
         self.assertTrue(expr=self._filter.filter(message))
@@ -30,7 +30,7 @@ class IsAdminFilterTestCase(TestCase):
     def test_filter_returns_false_if_user_id_is_not_admin(self) -> None:
         """Filter test if the message sender is not a bot administrator"""
         non_admin_id: int = 987654321
-        env.get_admin_ids = MagicMock(return_value=[self._admin_id])
+        env.get_admin_ids = MagicMock(return_value=[self._admin_id])  # type: ignore
         user: User = User(id=non_admin_id, first_name="Non-Admin", is_bot=False)
         message: Message = Message(message_id=1, from_user=user, date=self._date, chat=self._chat)
         self.assertFalse(expr=self._filter.filter(message))
